cdca710e9df84eecaf0a7b0a484a0b75744bdc20
barbara.sousa <barbara.sousa@coopersystem.com.br>
2020-05-25 15:54:25 -0300

    addin google search and send email

--- tars.py
+++ tars.py
@@ -1,12 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from gtts import gTTS
 import speech_recognition as sr 
 from pygame import mixer
 import random
+import re
+import webbrowser
+import smtplib
+import time
+
 
 def talk(audio):
   print(audio)
   for line in audio.splitlines():
-    text_to_speech = gTTS(text=audio, lang='en-uk')
+    text_to_speech = gTTS(text=audio, lang='en-us')
     text_to_speech.save('audio.mp3')
     mixer.init()
     mixer.music.load('audio.mp3')
@@ -20,7 +27,6 @@ def myCommand():
     r.pause_threshold = 2
     r.adjust_for_ambient_noise(source, duration=1)
     audio = r.listen(source)
-    print('dirgraça')
 
   try:
     command = r.recognize_google(audio).lower()
@@ -41,6 +47,46 @@ def tars(command):
 
   if 'hello' in command:
     talk('Hello! I\'m TARS. How can I help you')
+
+  if 'open google and search' in command:
+    reg_ex = re.search('open google (.*)', command)
+    search_for = command.split('search', 1)[1]
+    url = 'https://google.com'
+    if reg_ex:
+      subgoogle = reg_ex.group(1)
+      url = url + 'r/' + subgoogle
+  
+    talk('Okay!')
+    driver =  webdriver.Chrome()
+    driver.get('https://google.com')
+    search = driver.find_element_by_name('q')
+    search.send_keys(str(search_for))
+    search.send_keys(Keys.RETURN)
+
+    print('Done')
+
+  elif 'email' or 'gmail' in command:
+    talk('What is the subject?')
+    time.sleep(3)
+    subject = myCommand()
+    talk('What should I say?')
+    time.sleep(3)
+    message = myCommand()
+    content = 'Subject: {}\n\n{}'.format(subject, message)
+    
+    mail = smtplib.SMTP('smtp.gmail.com', 587)
+
+    mail.ehlo()
+
+    mail.starttls()
+
+    mail.login('your_email', 'your_password')
+
+    mail.sendmail('FROM', 'TO', content)
+
+    mail.close()
+
+    talk('Email sent!')
   
   else:
     error = random.choice(errors)
